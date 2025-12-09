e8744c920faed0b000c1767f71d1d78c4d1eeec3
Muhammad idris <idrismuhd814@gmail.com>
2025-12-09 18:19:28 +0100

    added UnitOfWork for database atomicity

---
+++ ProductOrder.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using ProductOrder.Application.Interfaces;
+using ProductOrder.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOrder.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ProductOrderDbContext _dbContext;
+        private IDbContextTransaction? _tx;
+        public UnitOfWork(ProductOrderDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _tx = await _dbContext.Database.BeginTransactionAsync(); // start a db transaction to ensure atomicity
+        }
+
+        public async Task CommitAsync()
+        {
+            if (_tx != null)
+            {
+                await _dbContext.SaveChangesAsync();
+                await _tx.CommitAsync(); _tx.Dispose(); _tx = null;
+            }
+        }
+        public async Task RollbackAsync()
+        {
+            if (_tx != null)
+            {
+                await _tx.RollbackAsync(); _tx.Dispose(); _tx = null;
+            }
+        }
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
