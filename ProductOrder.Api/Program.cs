83abe875d1640658ccc6f9e69e1a5b54dc151c07
Muhammad idris <idrismuhd814@gmail.com>
2025-12-11 07:04:32 +0100

    .

--- ProductOrder.Api/Program.cs
+++ ProductOrder.Api/Program.cs
@@ -9,8 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Configure for Render deployment
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // Add services to the container.
 
